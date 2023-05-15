7a0db5fe421c5e5d991efa38cb28e7fa684e8029
Marcos Lema <socramel@gmail.com>
2023-05-15 16:36:35 +0200

    añado db

--- app.js
+++ app.js
@@ -1,15 +1,54 @@
 import colors from "colors";
 
-import { inquirerMenu, pausa } from "./helpers/inquirer.js";
+import {
+  inquirerMenu,
+  pausa,
+  leerInput,
+  listadoTareasBorrar,
+} from "./helpers/inquirer.js";
 import Tareas from "./models/tareas.js";
+import { guardarDB, leerDB } from "./helpers/guardarArchivo.js";
 
 const main = async () => {
-  console.log("Hola mundo");
-
   let opt = "";
+  const tareas = new Tareas();
+
+  const tareasDB = leerDB();
+
+  if (tareasDB) {
+    tareas.cargarTareasFromArray(tareasDB);
+  }
+
   do {
+    // Imprime el menú
     opt = await inquirerMenu();
-    console.log({ opt });
+
+    switch (opt) {
+      case "1": // Crear opción
+        const desc = await leerInput("Descripción:");
+        tareas.crearTarea(desc);
+        break;
+
+      case "2":
+        tareas.listadoCompleto();
+        break;
+
+      case "3": // Listar completadas
+        tareas.listarPendientesCompletadas(true);
+        break;
+
+      case "4": // Listar pendientes
+        tareas.listarPendientesCompletadas(false);
+        break;
+
+      case "6": // Borrar
+        const id = await listadoTareasBorrar(tareas.listadoArr);
+        // Preguntar si está seguro
+        console.log({ id });
+        break;
+    }
+
+    guardarDB(tareas.listadoArr);
 
     await pausa();
   } while (opt !== "0");
--- models/tareas.js
+++ models/tareas.js
@@ -1,8 +1,68 @@
+import Tarea from "./tarea.js";
+
 class Tareas {
   _listado = {};
 
+  get listadoArr() {
+    const listado = [];
+    Object.keys(this._listado).forEach((key) => {
+      const tarea = this._listado[key];
+      listado.push(tarea);
+    });
+
+    return listado;
+  }
+
   constructor() {
     this._listado = {};
+  }
+
+  borrarTarea(id = "") {
+    if (this._listado[id]) {
+      delete this._listado[id];
+    }
+  }
+
+  cargarTareasFromArray(tareas = []) {
+    tareas.forEach((tarea) => {
+      this._listado[tarea.id] = tarea;
+    });
+  }
+
+  crearTarea(desc = "") {
+    const tarea = new Tarea(desc);
+    this._listado[tarea.id] = tarea;
+  }
+
+  listadoCompleto() {
+    console.log();
+    this.listadoArr.forEach((tarea, i) => {
+      const idx = `${i + 1}`.green;
+      const { desc, completadoEn } = tarea;
+      const estado = completadoEn ? "Completada".green : "Pendiente".red;
+      console.log(`${idx} ${desc} :: ${estado}`);
+    });
+  }
+  listarPendientesCompletadas(completadas = true) {
+    console.log();
+    let contador = 0;
+    this.listadoArr.forEach((tarea, i) => {
+      const { desc, completadoEn } = tarea;
+      const estado = completadoEn ? "Completada".green : "Pendiente".red;
+      if (completadas) {
+        // Mostrar completadas
+        if (completadoEn) {
+          contador += 1;
+          console.log(`${(contador + ".").green} ${desc} :: ${estado}`);
+        }
+      } else {
+        // Mostrar pendientes
+        if (!completadoEn) {
+          contador += 1;
+          console.log(`${(contador + ".").green} ${desc} :: ${estado}`);
+        }
+      }
+    });
   }
 }
 
